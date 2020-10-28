383f871540d25b4adf59011a670e6ee96376a194
Justin Moore <theoperaking@yahoo.com>
2020-10-27 20:46:32 -0400

    Add attack and run buttons and scripts

---
+++ Assets/Scripts/FighterAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterAction : MonoBehaviour
+{
+    private GameObject enemy;
+    private GameObject hero;
+
+    [SerializeField]
+    private GameObject meleePrefab;
+
+    [SerializeField]
+    private GameObject magicPrefab;
+
+    [SerializeField]
+    private Sprite faceIcon;
+
+    private GameObject currentAttack;
+    private GameObject meleeAttack;
+    private GameObject rangeAttack;
+
+    public void SelectAttack(string btn)
+    {
+        GameObject victim = hero;
+        if(tag == "Hero")
+        {
+            victim = enemy;
+        }
+        if (btn.CompareTo("melee") == 0)
+        {
+            Debug.Log("Melee attack");
+        } else if (btn.CompareTo("magic") == 0)
+        {
+            Debug.Log("Magic attack");
+        } else
+        {
+            Debug.Log("Run");
+        }
+    }
+}
